ef48a77b2b6629d78742df5c0c92241ff00e1845
GersonGmd <gersondiketama@yahoo.com>
2021-10-08 11:49:33 -0500

    clear input and update messages fixed

--- src/components/PublicMessages/SentMessages.js
+++ src/components/PublicMessages/SentMessages.js
@@ -5,25 +5,30 @@ import {AiOutlineSend} from "react-icons/ai"
 
 const SentMessages = ({getPublicMessages}) => {
   const [sentMessage, setSentMessage] = useState("");
-
+  
   const HandleInputAndSent = (event) => {
+   
+
     event.preventDefault();
     const messages = {
       post: sentMessage,
       userId: parseInt(sessionStorage.getItem("nutshell_user"))
     };
-
+    
     addPublicMessages(messages).then(() => 
-    getPublicMessages()
-    );
-  };
+    getPublicMessages())
+    
+    setSentMessage("")
+    }
+
 
   return (
     <>
       <input
         type="text"
-        placeholder=""
-        onChange={(evt) => setSentMessage(evt.target.value)}
+        placeholder="enter a message"
+        value={sentMessage}
+        onChange={(evt) => setSentMessage(evt.target.value) }
       />
       <button onClick={(event) => HandleInputAndSent(event)}><AiOutlineSend/></button>
     </>
--- src/components/PublicMessages/publicMessageManager.js
+++ src/components/PublicMessages/publicMessageManager.js
@@ -1,5 +1,13 @@
 const api = "http://localhost:8088";
 
+//get message by Id
+export const messagById = (messageInput) =>
+{
+  return fetch(`${api}/messages/${messageInput}`)
+  .then(response => response.json())
+}
+
+
 export const getPublicMessages = () => {
   return fetch(`${api}/messages`).then((response) => response.json());
 };
@@ -17,5 +25,17 @@ export const addPublicMessages = (newMessages) => {
 export const deleteMessages = (messageId) =>
 {
   return fetch(`${api}/messages/${messageId}`,{method:"DELETE"})
+  .then(response => response.json())
+}
+
+//edit
+
+export const editMessages = messageId =>
+{
+  return fetch(`${api}/messages/${messageId.id}`,{
+    method:"PUT",
+    headers: {"Content-Type":"application/json"},
+    body: JSON.stringify(messageId)
+  })
   .then(response => response.json())
 }
