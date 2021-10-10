97f2ebe78cc7e915933039c63fc2bf4bb63533ea
Susie Stanley <susiestanley00@gmail.com>
2021-10-10 14:22:31 -0500

    cleaned up Application Views spacing

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -37,8 +37,6 @@ export const ApplicationViews = () => {
         <EditForm />
       </Route>
 
-      {/* author: Gerson
-      Purpose: If the routh match the message edit path then will take the user to this page. */}
       <Route path="/messages/:messageId(\d+)/edit">
         <MessageEdit />
       </Route>
