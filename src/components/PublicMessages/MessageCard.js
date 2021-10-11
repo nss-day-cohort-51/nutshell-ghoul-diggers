0dd7b6c0d80b5121920849cac7bfab6de27f207d
Susie Stanley <susiestanley00@gmail.com>
2021-10-11 08:47:47 -0500

    updated styling

--- src/components/PublicMessages/MessageCard.js
+++ src/components/PublicMessages/MessageCard.js
@@ -2,7 +2,6 @@
 //Purpose: Card to display all the the message from API/ GET
 
 import React from 'react'
-import { Link } from "react-router-dom";
 import {FaEdit, FaTrash } from "react-icons/fa"
 import { useHistory } from 'react-router'
 
@@ -13,32 +12,31 @@ const MessageCard = ({data, handledelete}) => {
 <>
 <div className="card__content">
 
-<div className="message__info">
+        <div className="message__info">
 
-  <div><strong>Message: </strong>{data.post}</div>
-  <div><strong>Posted by: </strong> {data.userId} - {data.timestamp}</div>
+            <div><strong>Message: </strong>{data.post}</div>
+            <div><strong>Posted by: </strong> {data.userId} - {data.timestamp}</div>
 
-</div>
-
-<div className="remove__item">
-
-<Link to={`/messages/${data.id}/edit`}><button className="button sm"><FaEdit/></button></Link>
-
-    <button onClick={() =>handledelete(data.id)}> <FaTrash /></button>
-
-    <button onClick={() => history.push(`/messages/${data.id}/edit`)}><FaEdit/></button>
-
-    <button type="button" className="button sm" onClick={() => handledelete(data.id)}><FaTrash /></button>
-
-</div>
-
-</div>
-
-        <div className="message__container">
-            <h4>{data.post} 
-            
-            <button onClick={() =>handledelete(data.id)}><FaTrash /></button> <button onClick={() => history.push(`/messages/${data.id}/edit`)}><FaEdit/></button></h4>
         </div>
+
+        <div className="remove__item">
+
+            <button 
+            className="button sm"
+            onClick={() => history.push(`/messages/${data.id}/edit`)}>
+                <FaEdit/>
+            </button>
+
+            <button 
+            type="button" 
+            className="button sm" 
+            onClick={() => handledelete(data.id)}>
+                <FaTrash />
+            </button>
+
+        </div>
+
+</div>
 </>
     )
 }
